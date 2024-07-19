4767af415f018d10b7a99ac3e0e42daef7334522
ChayuthNol <chayuth.thana@gmail.com>
2024-07-19 15:26:27 +0700

    Add files via upload

--- 02_PMSM/Speed Control/PMSM_parameter2.m
+++ 02_PMSM/Speed Control/PMSM_parameter2.m
@@ -1,5 +1,5 @@
 R = 0.2;
-w_ref =4000;
+w_ref =3000;
 Lamda = 0.1194;
 Ls = 0.0003752 ;
 Ld = 0.0003752 ;
@@ -7,6 +7,10 @@ Lq = 0.0003752 ;
 T_load = 10;
 Pole = 2;
 J_motor = 0.0027;
-w_base =2400; 
-v_rated = 30;
+
+v_rated1 = 30;
+w_base =(60/(2*pi))*(v_rated/sqrt((Ls^2)*(T_load/1.5*Pole*Lamda)^2+(Lamda^2)));
+%v_rated = 28;
+%w_base = 2400;
+
 T_ref = 10;
